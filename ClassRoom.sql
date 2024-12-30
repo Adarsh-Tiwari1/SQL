5a9da0883fa328c999d5459ef3cfec9101946c66
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-30 16:14:02 +0530

    Some Code in SQL

---
+++ ClassRoom.sql
@@ -0,0 +1,16 @@
+create database college;
+-- create database if not exists college;
+-- drop database if exists company;
+use college;
+create table student(
+	id int primary key,
+    name varchar(50),
+    age int not null
+);
+show databases;
+show table status;
+show tables;
+
+
+
+
